264ffc34b7875a7985e323dcfaeec7d071fded48
simonebonifazi <simonbonifraz@gmail.com>
2022-12-28 18:37:08 +0100

    added and tested third query

--- dao/playerDao.py
+++ dao/playerDao.py
@@ -29,3 +29,17 @@ class PlayerDao:
             f"SELECT * FROM mondiali.giocatore join partecipazione p on p.IDGiocatore where Anno = {year}")
         data = MySql.getResults()
         return data
+
+    @classmethod
+    def getPlayerWhoHadPlayedInThreeWorldCupOrInMoreThanOneTeam(cls):
+        MySql.openConnection()
+        MySql.query(
+            "select	Nome \
+            from Giocatore	G \
+            where	3=(select	count(*) \
+                     from Partecipazione	where	IDGiocatore=G.ID) \
+            or 1 < (select count(distinct Nazione) \
+                    from Partecipazione \
+                    where	IDGiocatore=G.ID)")
+        data = MySql.getResults()
+        return data
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from dao.partecipationDao import PartecipationDao
 from dao.organizationDao import OrganizationDao
 from dao.playerDao import PlayerDao
 from dao.teamDao import TeamDao
-tester = TeamDao.getTeamWithMostPlayerConvocatedPerYear()
-print(tester[0])
+tester = PlayerDao.getPlayerWhoHadPlayedInThreeWorldCupOrInMoreThanOneTeam()
+print(tester)
