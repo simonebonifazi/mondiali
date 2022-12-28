6ad2399d26062e7b9d33770857cea482b401eccb
simonebonifazi <simonbonifraz@gmail.com>
2022-12-28 16:35:19 +0100

    added query on playerDao

--- dao/playerDao.py
+++ dao/playerDao.py
@@ -8,3 +8,18 @@ class PlayerDao:
         MySql.query("select * from giocatore")
         data = MySql.getResults()
         return data
+
+    @classmethod
+    def getPlayerByID(cls, id):
+        MySql.openConnection()
+        MySql.query(f"select * from giocatore where ID = {id}")
+        data = MySql.getResults()
+        return data
+
+    @classmethod
+    def getAllPlayerPerYear(cls, year):
+        MySql.openConnection()
+        MySql.query(
+            f"SELECT * FROM mondiali.giocatore join partecipazione p on p.IDGiocatore where Anno = {year}")
+        data = MySql.getResults()
+        return data
