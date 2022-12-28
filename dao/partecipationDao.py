7c5c24606f473681d70d963b90a8ef82723aad30
simonebonifazi <simonbonifraz@gmail.com>
2022-12-28 17:45:40 +0100

    ended partecipation dto in dao, still to test

--- dao/partecipationDao.py
+++ dao/partecipationDao.py
@@ -1,4 +1,5 @@
 from dao.utility.db import MySql
+from dto.Partecipation import Partecipation
 
 
 class PartecipationDao:
@@ -7,4 +8,8 @@ class PartecipationDao:
         MySql.openConnection()
         MySql.query("select * from partecipazione")
         data = MySql.getResults()
-        return data
+        partecipation = []
+        for element in data:
+            partecipation.append(Partecipation(
+                element[0], element[1], element[2], element[3]))
+        return partecipation
